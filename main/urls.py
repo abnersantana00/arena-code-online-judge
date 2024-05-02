237ee5c6c373c7a07a4d9ea42cbdce171f34e470
Abner de Santana Silva <100228850+abnersantana00@users.noreply.github.com>
2024-05-02 14:37:27 -0300

    main page
    main page simple, but need alterations in front end.

--- main/urls.py
+++ main/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import home
+from .views import home, topic
 
 urlpatterns = [
     path('', home, name='home'),
+    path('<str:topic_id>/', topic, name='topic'),
 ]
--- main/views.py
+++ main/views.py
@@ -3,8 +3,12 @@ from django.shortcuts import render
 # Create your views here.
 
 from django.shortcuts import render
-
+from django.http import HttpResponse
 
 def home(request):
 
     return render(request, 'main/home.html')
+
+def topic(request, topic_id):
+
+    return render(request, 'topic/.html')
