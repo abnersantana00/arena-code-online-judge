1b5ee3212762e57eb7f8e0c3a13e83688bacd340
Abner de Santana Silva <100228850+abnersantana00@users.noreply.github.com>
2024-05-26 15:22:34 -0300

    back end its work, need front end plase

--- main/urls.py
+++ main/urls.py
@@ -1,10 +1,12 @@
 # urls.py
 from django.urls import path
+from django.views.generic import RedirectView
 from .views import home, topic, topic_detail, source_code
 
 urlpatterns = [
+    path('favicon.ico/', RedirectView.as_view(url='/static/favicon.ico', permanent=True)),
     path('', home, name='home'),
     path('<str:topic>/', topic, name='topic'),  # Ajustar o nome do argumento para topic_name
-    path('<str:topic>/<str:title_name>/', topic_detail, name='topic_detail'),
-    path('<str:topic>/<str:title_name>/source-code/', source_code, name='source_code'),
+    path('<str:topic>/<str:topic_name>/', topic_detail, name='topic_detail'),
+    path('<str:topic>/<str:topic_name>/<str:problem_id>/source-code/', source_code, name='source_code'),
 ]
--- main/views.py
+++ main/views.py
@@ -1,16 +1,8 @@
 from django.shortcuts import render
-
-# Create your views here.
-
-from django.shortcuts import render
-from django.http import HttpResponse
-from django.shortcuts import render
 from django.views.decorators.csrf import csrf_protect
 from .utils import extract_titles, generate_all_questions, generate_one_question, run_test
 import json
-import os
 def home(request):
-
     return render(request, 'home.html')
 
 
@@ -21,7 +13,7 @@ def topic(request, topic):
 
     if topic == 'conditional':
          titles_info = extract_titles('main/static/json-files/templates/conditional.json')
-         context = {'topic' : 'Contitionals',
+         context = {
                     'titles_info': titles_info,
                     'topic': topic,
                     }
@@ -31,12 +23,11 @@ def topic(request, topic):
          return render(request, 'topic.html', context)
 
 
-def topic_detail(request, topic, title_name):
-    # Construindo os caminhos dos arquivos com base no topic_name
+def topic_detail(request, topic, topic_name):
+
     file1 = f'main/static/json-files/templates/{topic}.json'
     file2 = f'main/static/json-files/questions/{topic}-questions.json'
 
-    # Lendo os arquivos JSON
     with open(file1, 'r', encoding='utf-8') as file:
         json_template = json.load(file)
     with open(file2, 'r', encoding='utf-8') as file:
@@ -44,13 +35,14 @@ def topic_detail(request, topic, title_name):
 
     all_questions = generate_all_questions(json_template,json_questions )
     one_question = generate_one_question(json_template, json_questions, "102024")
-
+    problem_id = one_question['problem_id']
 
     context = {
-        'title_name': title_name,
         'topic': topic,
+        'topic_name': topic_name,
         'all_questions': all_questions,
-        'one_question' : one_question
+        'one_question' : one_question,
+        'problem_id' : problem_id
     }
 
 
@@ -59,35 +51,21 @@ def topic_detail(request, topic, title_name):
 
 
 
-def source_code(request, topic, title_name):
-    code = request.POST.get('code')
-    print("------- codes ----", code)
+def source_code(request, topic, topic_name, problem_id):
     if request.method == 'POST':
-        code = request.POST.get('code', '')  # Obtém o código do request.POST
-        print("source does CODE  : ", code)
-        source_code = request.POST.get('source_code')
-        topic_id = request.POST.get('topic_id')
-        problem_id = request.POST.get('problem_id')
-
+        code_submission = request.POST.get('code_submission')
         file2 = f'main/static/json-files/questions/{topic}-questions.json'
         with open(file2, 'r', encoding='utf-8') as file:
             json_questions = json.load(file)
 
-        result = run_test(source_code, json_questions,problem_id)
+        result = run_test(code_submission, json_questions,problem_id)
         print("RESULTADO DO JUIZ :", result)
-        print("SOURCE CODE >>>> ",source_code)
-        # Aqui você pode processar o código-fonte enviado e o objeto one_question
-        # Por exemplo, salvar em um banco de dados, executar testes, etc.
-        # Para simplicidade, vamos apenas passar esses dados para o contexto
-        print("run_test :", type(source_code), source_code, type(json_questions), type(problem_id), problem_id)
+
         context = {
             'topic': topic,
-            'title_name': title_name,
-            'source_code': source_code,
-            'topic_id': topic_id,
-            'problem_id': problem_id,
+            'topic_name': topic_name,
+            'code_submission': code_submission,
             'result' : result,
-            'code': code,
         }
 
         return render(request, 'topic_source_code.html', context)
@@ -97,21 +75,3 @@ def source_code(request, topic, title_name):
 
 
 
-def source_code(request, title_name):
-
-    if request.method == 'POST':
-        code = request.POST.get('code', '')
-        problem_id = request.POST.get('problem_id')
-        file2 = f'main/static/json-files/questions/{topic}-questions.json'
-        with open(file2, 'r', encoding='utf-8') as file:
-            json_questions = json.load(file)
-        result = run_test(source_code, json_questions,problem_id)
-        context = {
-            'title_name': title_name,
-            'source_code': source_code,
-            'result': result,
-            'code': code,
-        }
-        return render(request, 'topic_source_code.html', context)
-
-    return render(request, 'topic_source_code.html')
